b9dfe8c3fb9a1bfb15a0a2d0bfb31d110f28d0a5
akiko777 <natsu.hi.boshi731rainbowrose@gmail.com>
2021-04-14 23:01:19 +0900

    投稿情報編集・更新機能（ビュー以外）

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   #get 'shares/index'
   devise_for :users
   root to: 'shares#index'
-  resources :shares, only: [:index, :new, :create]
+  resources :shares, only: [:index, :new, :create, :edit, :update]
 end
