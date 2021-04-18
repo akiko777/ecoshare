32975943500c22273ed532bd3d41a21fd02de8e4
akiko777 <natsu.hi.boshi731rainbowrose@gmail.com>
2021-04-18 17:48:03 +0900

    ルーティング設定、commentコントローラー作成

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,7 @@ Rails.application.routes.draw do
   #get 'shares/index'
   devise_for :users
   root to: 'shares#index'
-  resources :shares, only: [:index, :new, :create, :show, :edit, :update, :destroy]
+  resources :shares, only: [:index, :new, :create, :show, :edit, :update, :destroy] do
+    resources :comments, only: [:create]
+  end
 end
