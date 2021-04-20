d1ca56f4de025097f2a07be38f75b1465ea4cd25
akiko777 <natsu.hi.boshi731rainbowrose@gmail.com>
2021-04-20 19:48:26 +0900

    コメント機能　修正

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -3,14 +3,16 @@ class CommentsController < ApplicationController
   def create
     @comment = Comment.new(comment_params)
     if @comment.save
-      redirect_to share_path(@comment.share)
+      redirect_to share_path(@comment.share), notice: 'コメントしました'
     else
       @share = @comment.share
       @comments = @share.comments
+      flash.now[:alert] = 'コメントを入力してください'
       render "shares/show"
     end
   end
 
+  
 
   private
   def comment_params
