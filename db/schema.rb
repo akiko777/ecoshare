743968269dfe3a2430a52808b6480d5b3402502b
akiko777 <natsu.hi.boshi731rainbowrose@gmail.com>
2021-04-22 23:46:05 +0900

    likesテーブルを作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_04_18_070844) do
+ActiveRecord::Schema.define(version: 2021_04_22_143934) do
 
   create_table "active_storage_attachments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
@@ -43,6 +43,15 @@ ActiveRecord::Schema.define(version: 2021_04_18_070844) do
     t.index ["user_id"], name: "index_comments_on_user_id"
   end
 
+  create_table "likes", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.bigint "user_id"
+    t.bigint "share_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["share_id"], name: "index_likes_on_share_id"
+    t.index ["user_id"], name: "index_likes_on_user_id"
+  end
+
   create_table "shares", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "title", null: false
     t.text "share", null: false
@@ -69,5 +78,7 @@ ActiveRecord::Schema.define(version: 2021_04_18_070844) do
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "comments", "shares"
   add_foreign_key "comments", "users"
+  add_foreign_key "likes", "shares"
+  add_foreign_key "likes", "users"
   add_foreign_key "shares", "users"
 end
