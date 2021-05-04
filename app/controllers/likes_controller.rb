60520bcff657a59e8ba9db3b1ef4974bf953d897
akiko777 <natsu.hi.boshi731rainbowrose@gmail.com>
2021-05-04 15:10:42 +0900

    request.refererの記述を修正

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -2,7 +2,7 @@ class LikesController < ApplicationController
 
   def create
     @like = Like.create(user_id: current_user.id, share_id: params[:share_id])
-    if request.referer == "http://localhost:3000/shares" 
+    if URI(request.referer).path == "/shares" 
       redirect_to shares_path, notice: "いいねしました！"    
     else
       redirect_to share_path(@like.share.id), notice: "いいねしました！" 
@@ -14,7 +14,7 @@ class LikesController < ApplicationController
     if like.present?
       like.destroy
     end 
-    if  request.referer == "http://localhost:3000/shares"
+    if  URI(request.referer).path == "/shares"
       redirect_to shares_path, notice: "いいねを取り消しました！" 
     else 
       redirect_to share_path(like.share.id), notice: "いいねを取り消しました！" 
